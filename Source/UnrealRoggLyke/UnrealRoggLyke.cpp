58e4fcec6b4e3beaf589c9bfdcabc1fc24ff59a7
lenowski <stanislaw.len@proton.me>
2026-08-08 20:22:55 +0200

    Initial commit

---
+++ Source/UnrealRoggLyke/UnrealRoggLyke.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealRoggLyke.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealRoggLyke, "UnrealRoggLyke" );
